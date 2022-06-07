3eaf77e0933e4e2f0fa38a0bd93f80be37252c1c
unknown <leonardoxavier092@gmail.com>
2022-06-07 19:19:19 -0300

    first commit

---
+++ tests/firstTest.spec.ts
@@ -0,0 +1,11 @@
+describe("First Test", () => {
+  describe("When x = 1 and y = 2", () => {
+    it("returns 3", () => {
+      const x = 1;
+      const y = 2;
+
+      const sum = x + y;
+      expect(sum).toBe(3);
+    });
+  });
+});
---
+++ tests/integration/categories.spec.ts
@@ -0,0 +1,17 @@
+import app from "@src/app";
+import request from "supertest";
+
+describe("Categories", () => {
+  describe("GET /categories", () => {
+    it("Return a list of categories in JSON format", () => {
+      const expectedJson = JSON.stringify([
+        {
+          id: 1,
+          name: "Jogos",
+        },
+      ]);
+
+      return request(app).get("/categories").expect(200).expect(expectedJson);
+    });
+  });
+});
