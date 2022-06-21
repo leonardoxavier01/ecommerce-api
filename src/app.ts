512b6de3fc805c106767270d9c4bbc966a79ab2d
Leonardo Xavier <95321400+leonardoxavier01@users.noreply.github.com>
2022-06-21 19:34:11 -0300

    features catgories (#3)
    * fix: diretory setup-prisma
    
    * fix: prettier
    
    * test: fixes all tests

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,20 @@
+import "dotenv/config";
 import express from "express";
 import { Request, Response } from "express";
+import { find } from "@src/services/categoryService";
 
 const app = express();
 
-app.get("/categories", (req: Request, res: Response) => {
-  res.json([
-    {
-      id: 1,
-      name: "Jogos",
-    },
-  ]);
+app.get("/categories", async (req: Request, res: Response) => {
+  const categories = await find();
+
+  res.json(categories);
+});
+
+app.post("/admin/categories", async (req: Request, res: Response) => {
+  console.log(req);
+
+  res.json({});
 });
 
 export default app;
