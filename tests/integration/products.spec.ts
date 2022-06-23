8a2edc5a331592ab84a9f1db33c2d36932857afd
unknown <leonardoxavier092@gmail.com>
2022-06-23 20:43:57 -0300

    feat: products

--- tests/integration/products.spec.ts
+++ tests/integration/products.spec.ts
@@ -1,20 +1,20 @@
 import { createCategory, createProduct } from "@tests/factories";
 import { clearTables } from "@tests/database";
-import { Prisma } from "@prisma/client";
 
 import app from "@src/app";
 import request from "supertest";
 
-type Product = Prisma.ProductGetPayload<{}>;
-
 describe("Products", () => {
-  describe.only("GET /categories/:id/products", () => {
-    let product1: Product;
-    let product2: Product;
+  describe("GET /categories/:id/products", () => {
+    let product1;
+    let product2;
+    let categoryId: string;
     beforeAll(async () => {
       await clearTables();
 
       const category = await createCategory({ name: "Perifericos" });
+      categoryId = category.id;
+
       product1 = await createProduct({
         name: "Mouse",
         categoryId: category.id,
@@ -29,33 +29,43 @@ describe("Products", () => {
       await clearTables();
     });
 
-    it(" return a list of products from that category", async () => {
-      const client = request(app);
-      const res = await client.get("/categories/:id/products").expect(200);
+    it("returns a list of products from that category", async () => {
+      const res = await request(app)
+        .get(`/categories/${categoryId}/products`)
+        .expect(200);
 
       const [item1, item2] = res.body;
 
-      expect(product1.name).toBe("Mouse");
-      expect(product2.name).toBe("Teclado");
+      console.log(res.body);
+
+      expect(item1.name).toBe("Mouse");
+      expect(item2.name).toBe("Teclado");
     });
   });
+
   describe("GET /products/:id", () => {
-    let product1;
+    let productId: string;
 
     beforeAll(async () => {
       await clearTables();
+
       const category = await createCategory({ name: "Perifericos" });
-      product1 = createProduct({ name: "Mouse", categoryId: category.id });
+      const product = await createProduct({
+        name: "Mouse",
+        categoryId: category.id,
+      });
+
+      productId = product.id;
     });
 
     afterAll(async () => {
       await clearTables();
     });
 
-    it(" return a single product based on that id", async () => {
+    it("returns a single product based on that id", async () => {
       const client = request(app);
 
-      const res = await client.get("/products/:id").expect(200);
+      const res = await client.get(`/products/${productId}`).expect(200);
 
       const item = res.body;
       expect(item.name).toBe("Mouse");
