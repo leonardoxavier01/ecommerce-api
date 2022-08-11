c8cabf634d240c6ea8c47c9a8efed0d784fdff29
Leonardo -Software Developer <leonardoxavier092@gmail.com>
2022-08-10 20:19:42 -0400

    test: cors

--- src/app.ts
+++ src/app.ts
@@ -15,10 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "https://boiling-earth-73197.herokuapp.com/",
-      "http://localhost:3000",
-    ],
+    origin: ["*"],
   })
 );
 
